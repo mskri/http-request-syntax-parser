584795846e94db0400105901ba84d08b25ac724d
Sakari Mursu <mskri@mursu.dev>
2022-11-05 21:22:27 +0200

    Parse headers

--- src/httpRequestParser.ts
+++ src/httpRequestParser.ts
@@ -1,4 +1,5 @@
 import { EOL } from 'os';
+import * as http from 'http';
 
 const DEFAULT_METHOD = 'GET';
 const defaultVersion = 'HTTP/1.1';
@@ -7,6 +8,11 @@ const defaultVersion = 'HTTP/1.1';
 // Matches lines starting with 0 or more whitespace followed by `&`, `?` or `/`
 const LONG_REQUEST_LINE_BREAK_PREFIX = /^\s*[&\?/]/;
 
+// https://www.rfc-editor.org/rfc/rfc9110.html#name-field-names
+const HEADER_LINE_FIELD_NAME = /^[a-zA-Z-_]+$/;
+
+// https://www.rfc-editor.org/rfc/rfc9110.html#name-field-values
+const HEADER_LINE_FIELD_VALUE = /^[a-zA-Z0-9_ :;.,\\\/"'?!(){}[\]@<>=\-+*#$&`|~^%]*$/;
 enum ParseState {
   RequestLine,
   Header,
@@ -20,12 +26,10 @@ type Method = typeof methods[number];
 type HttpRequest = {
   method: Method;
   pathname: string;
+  headers: Headers;
 };
 
-type RequestLine = {
-  method: Method;
-  url: string;
-};
+type Headers = http.OutgoingHttpHeaders;
 
 /**
  * Parses HTTP request from string into object.
@@ -89,8 +93,67 @@ export function parseHttpRequest(message: string): HttpRequest {
     }
   }
 
+  const headers = parseHeaders(headersLines);
+
   return {
     method: 'CONNECT',
     pathname: 'placeholder',
+    headers,
   };
+}
+
+/**
+ * Parse header lines into headers.
+ *
+ * Header line format:
+ *  field-name = field-value
+ *
+ * @param {Array<string>} headerLines Headers parsed into a array
+ * @returns {Headers} Parsed headers
+ */
+function parseHeaders(headerLines: string[]): Headers {
+  // message-header = field-name ":" [ field-value ]
+  const headers: Headers = {};
+  const headerNames: { [key: string]: string } = {};
+
+  headerLines.forEach((headerLine) => {
+    const [name, ...rest] = headerLine.split(':');
+    const fieldName: string = name;
+    const fieldValue: string = rest.join(':').trim();
+
+    if (!HEADER_LINE_FIELD_NAME.test(fieldName)) {
+      throw new HttpParseError('Header field-name is not valid');
+    }
+
+    if (!HEADER_LINE_FIELD_VALUE.test(fieldValue)) {
+      throw new HttpParseError('Header field-value is not valid');
+    }
+
+    const normalizedFieldName = fieldName.toLowerCase();
+
+    // Checking existing normalized field-name because repeated headers should be concatenated in a
+    // list where values are separated by comma. Cookie values are separated by semicolon.
+    // https://www.rfc-editor.org/rfc/rfc9110.html#name-field-lines-and-combined-fi
+    const headerName = headerNames[normalizedFieldName];
+    if (!headerName) {
+      headerNames[normalizedFieldName] = fieldName;
+      headers[fieldName] = fieldValue;
+    } else {
+      const headerName = headerNames[normalizedFieldName];
+      const separator = normalizedFieldName === 'cookie' ? ';' : ',';
+      const value = headers[headerName]?.toString();
+      const existingValues = value?.split(separator)?.map((v) => v.trim()) ?? [];
+      headers[headerName] = [...existingValues, fieldValue].join(`${separator} `);
+    }
+  });
+
+  return headers;
+}
+
+export class HttpParseError extends Error {
+  constructor(message: string) {
+    super();
+    this.name = 'HttpParseError';
+    this.message = message;
+  }
 }
